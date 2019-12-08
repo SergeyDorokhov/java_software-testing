8a50b7fef25b4d7e04f8d5e3c359abd78026fd3f
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2019-12-08 11:33:08 +0300

    Добавлена возможность запуска тестов в браузерах IE, Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,16 +2,25 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    public FirefoxDriver wd;
+    public WebDriver wd;
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private int browser;
+
+    public ApplicationManager (int browser) {
+        this.browser = browser;
+    }
 
     public SessionHelper getSessionHelper() {
         return sessionHelper;
@@ -30,7 +39,14 @@ public class ApplicationManager {
     }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser == BrowserType.MOZILLA) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+            wd = new InternetExplorerDriver();
+        } else {
+            wd =new ChromeDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
