8f33c51ed72a470915960161271ae3613aada88a
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2019-12-09 19:07:32 +0300

    Изменен импорт класса BrowserType и добавлено сравнение через equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +16,9 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
-    private int browser;
+    private String browser;
 
-    public ApplicationManager (int browser) {
+    public ApplicationManager (String browser) {
         this.browser = browser;
     }
 
@@ -39,9 +39,9 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.MOZILLA) {
+        if (browser.equals(BrowserType.FIREFOX) ) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         } else {
             wd =new ChromeDriver();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import sun.plugin2.util.BrowserType;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.DEFAULT);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
@@ -18,5 +18,4 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.stop();
     }
-
 }
