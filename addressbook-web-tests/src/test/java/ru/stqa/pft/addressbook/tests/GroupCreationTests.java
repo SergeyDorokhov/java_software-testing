773593829ebbd818efac2647824980606614186c
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2019-12-16 21:43:18 +0300

    Реализована сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,31 +3,26 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.getGroupHelper().gotoGroupsPage();
-    List<GroupData> groupsBefore = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().initGroupCreation();
-    GroupData newGroup = new GroupData("111", null, null);
-    app.getGroupHelper().fillGroupForm(newGroup);
-    app.getGroupHelper().returnToGroupPage();
-    List<GroupData> groupsAfter = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(groupsAfter.size(), groupsBefore.size() + 1);
-    int max = 0;
-    for (GroupData group: groupsAfter) {
-      if (max < group.getId()) {
-        max = group.getId();
-      }
+    @Test
+    public void testGroupCreation() throws Exception {
+        app.getGroupHelper().gotoGroupsPage();
+        List<GroupData> groupsBefore = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().initGroupCreation();
+        GroupData newGroup = new GroupData("111", null, null);
+        app.getGroupHelper().fillGroupForm(newGroup);
+        app.getGroupHelper().returnToGroupPage();
+        List<GroupData> groupsAfter = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(groupsAfter.size(), groupsBefore.size() + 1);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        groupsAfter.sort(byId);
+        groupsBefore.add(groupsAfter.get(groupsAfter.size() - 1));
+        groupsBefore.sort(byId);
+        Assert.assertEquals(groupsBefore, groupsAfter);
+        app.getSessionHelper().logout();
     }
-    newGroup.setId(max);
-    groupsBefore.add(newGroup);
-    Assert.assertEquals(new HashSet<>(groupsAfter), new HashSet<>(groupsBefore));
-    app.getSessionHelper().logout();
-  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -3,8 +3,7 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -19,14 +18,17 @@ public class GroupModificationTest extends TestBase {
         app.getGroupHelper().selectGroup(groupsBefore.size() - 1);
         app.getGroupHelper().modificationSelectedGroup();
         GroupData modificationGroup = new GroupData(groupsBefore.get(groupsBefore.size() - 1).getId(),
-                "111","111","222");
+                "111", "111", "222");
         app.getGroupHelper().fillGroupForm(modificationGroup);
         app.getGroupHelper().gotoGroupsPage();
         List<GroupData> groupsAfter = app.getGroupHelper().getGroupList();
         Assert.assertEquals(groupsAfter.size(), groupsBefore.size());
         groupsBefore.remove(groupsBefore.size() - 1);
         groupsBefore.add(modificationGroup);
-        Assert.assertEquals(new HashSet<>(groupsAfter), new HashSet<>(groupsBefore));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        groupsAfter.sort(byId);
+        groupsBefore.sort(byId);
+        Assert.assertEquals(groupsAfter, groupsBefore);
         app.getSessionHelper().logout();
     }
 }
