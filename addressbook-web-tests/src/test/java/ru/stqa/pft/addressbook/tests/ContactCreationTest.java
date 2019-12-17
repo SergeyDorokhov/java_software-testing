3423b53c5690d5dd99a6050e6c71e289e3da7d09
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2019-12-17 22:05:14 +0300

    Тест на создание контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,17 +1,30 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class ContactCreationTest extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    app.getContactHelper().gotoContactCreationPage();
-    app.getContactHelper().fillContactCreationForm(new ContactData("AAAAA", "BBBBBB", "CCCCCC",
-            "Z", "Moscow", "89999999999", "aaa@mail.ru", "111"), true);
-    app.getContactHelper().submitContactCreation();
-    app.getNavigationHelper().gotoHomePage();
-    app.getSessionHelper().logout();
-  }
+    @Test
+    public void testContactCreation() throws Exception {
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().gotoContactCreationPage();
+        ContactData newContact = new ContactData("AAAAA", "BBBBBB", "CCCCCC",
+                "Z", "Moscow", "89999999999", "aaa@mail.ru", "111");
+        app.getContactHelper().fillContactCreationForm(newContact, true);
+        app.getContactHelper().submitContactCreation();
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        newContact.setId(after.get(after.size() - 1).getId());
+        before.add(newContact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        after.sort(byId);
+        before.sort(byId);
+        Assert.assertEquals(before, after);
+        app.getSessionHelper().logout();
+    }
 }
